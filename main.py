c70f3c49826463110953f0e5dc7411ca5c5e631d
diyorbek6875 <diyorbekabdusalomov32@gmail.com>
2023-07-27 14:37:34 +0500

    exo_bot_in_python_telegram_bot

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
 import telegram
+from telegram.ext import Updater, MessageHandler, Filters
 import os 
 
 TOKEN = os.environ['TOKEN']
 
-bot = telegram.Bot(TOKEN)
+# update = bot.getUpdates()[-1]
 
-update = bot.getUpdates()[-1]
+# chat_id = update.message.chat.id
 
-chat_id = update.message.chat.id
+# bot.sendMessage(chat_id, "123")
+def main(update,context):
+    chat_id=update.message.chat.id
+    text=update.message.text
+    print(text)
+    bot=context.bot
+    bot.sendMessage(chat_id=chat_id,text=text)
+    
+    
+updater=Updater(TOKEN)
+dp=updater.dispatcher
 
-bot.sendMessage(chat_id, "123")
+dp.add_handler(MessageHandler(Filters.text, main))
+
+updater.start_polling()
+updater.idle()
